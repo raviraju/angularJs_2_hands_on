7928cf42a4b9984bc0bc3e7c9e0bd41f0bec7a46
raviraju <ravirajuk.raju@gmail.com>
2016-06-13 09:39:25 +0530

    AngularJS_2 essential training handson

---
+++ app/media-item-form.component.ts
@@ -0,0 +1,53 @@
+import {Component, Inject} from 'angular2/core';
+import {FormBuilder, Control, Validators} from 'angular2/common';
+import {MediaItemService} from './media-item.service';
+import {LOOKUP_LISTS} from './providers';
+import {Router} from 'angular2/router';
+
+@Component({
+    selector: 'media-item-form',
+    templateUrl: 'app/media-item-form.component.html',
+    styleUrls: ['app/media-item-form.component.css']
+})
+export class MediaItemFormComponent {
+    form;
+
+    constructor(private formBuilder: FormBuilder,
+            private mediaItemService: MediaItemService,
+            @Inject(LOOKUP_LISTS) public lookupLists,
+            private router: Router){}
+
+    ngOnInit(){
+        //this.form = new ControlGroup({
+        this.form = this.formBuilder.group({
+            'medium' : new Control('Movies'),
+            'name': new Control('', Validators.compose([
+                Validators.required,
+                Validators.pattern('[\\w\\-\\s\\/]+')
+            ])),
+            'category': new Control(''),
+            'year': new Control('', this.yearValidator)
+        })
+    }
+
+    yearValidator(control){
+        if(control.value.trim().length === 0) return null;
+        var year = parseInt(control.value);
+        var minYear = 1800;
+        var maxYear = 2500;
+        if(year>= minYear && year<= maxYear) return null;
+        return {'year' : {
+                            'min' : minYear, 
+                            'max' : maxYear
+                        }
+                };
+    }
+
+    onSubmit(mediaItem){
+        console.log(mediaItem);
+        this.mediaItemService.add(mediaItem)
+            .subscribe(() => {
+                this.router.navigate(['../List', {medium: mediaItem.medium}])
+            });
+    }
+}
---
+++ app/media-item.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {FavoriteDirective} from './favorite.directive';
+
+@Component({
+    selector: 'media-item',
+    directives: [FavoriteDirective],
+    templateUrl: 'app/media-item.component.html',
+    styleUrls: ['app/media-item.component.css']
+})
+export class MediaItemComponent {
+    @Input('mediaItemToWatch') mediaItem;
+    @Output('deleted') delete = new EventEmitter();
+    
+    onDelete(){
+        console.log("onDelete");
+        this.delete.emit(this.mediaItem);
+        console.log("emitted");
+    }
+}
